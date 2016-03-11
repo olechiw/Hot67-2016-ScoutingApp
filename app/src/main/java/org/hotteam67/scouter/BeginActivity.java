c09eb6741e0ae7d623d43b1467e3deb7e04cef7b
pianist1119 <pianist1119@gmail.com>
2016-03-11 18:49:45 -0500

    Almost done with UI, and added a custom toggleableradiobutton class which should allow for a toggleable button.

--- app/src/main/java/org/hotteam67/scouter/BeginActivity.java
+++ app/src/main/java/org/hotteam67/scouter/BeginActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class BeginActivity extends AppCompatActivity {
 
@@ -15,15 +16,6 @@ public class BeginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_begin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
